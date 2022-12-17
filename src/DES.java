cb96da5ca0775610ca80cb0e7042b57a28e3b4ee
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-18 02:17:29 +0300

    -

--- src/DES.java
+++ src/DES.java
@@ -6,10 +6,29 @@ public class DES {
 
 
     public String encrypt(String str) {
+        
+        
+        
+        
+        
         return null;
     }
 
     public String decrypt(String str) {
+        return null;
+    }
+    
+    
+    private String[] splitToBlocks(String str) {
+        int blocksAmount = str.length() / 8;
+        int addedAmount = str.length() - blocksAmount * 8;
+        String[] result = new String[blocksAmount];
+
+//        for (int i = 0; i < ; i++) {
+//
+//        }
+        
+        
         return null;
     }
 
