0ea5ed0fc46b0be9fb167b644239f9f41dd8ceb5
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-18 22:37:17 +0300

    -

--- src/DES.java
+++ src/DES.java
@@ -108,9 +108,19 @@ public class DES {
     // Расширение блока до размера в 48 бит.
     private boolean[] ext(boolean[] booleans) {
         assert booleans.length == 32 : "Длина блока для расширения должна быть 32 бита";
-        boolean[] result = new boolean[48];
 
-        return result;
+        byte[] mixVector = new byte[] {
+                32,  1,  2,  3,  4,  5,
+                 4,  5,  6,  7,  8,  9,
+                 8,  9, 10, 11, 12, 13,
+                12, 13, 14, 15, 16, 17,
+                16, 17, 18, 19, 20, 21,
+                20, 21, 22, 23, 24, 25,
+                24, 25, 26, 27, 28, 29,
+                28, 29, 30, 31, 32,  1
+        };
+
+        return Utility.mix(booleans, 48, mixVector);
     }
 
 
