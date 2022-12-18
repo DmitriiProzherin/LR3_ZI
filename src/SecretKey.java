5022b09aafd57485d364805418abeb9f9934f738
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-18 22:41:32 +0300

    -

--- src/SecretKey.java
+++ src/SecretKey.java
@@ -15,7 +15,7 @@ public class SecretKey {
     }
 
     private boolean[] initTransform(boolean[] in) {
-        assert in.length == 64 : "Длина входного ключа должна быть 64 бита.";
+        assert in.length == 64 : "Длина входного ключа для должна быть 64 бита.";
 
         byte[] mixVector = new byte[] {
                 57, 49, 41, 33, 25, 17,  9,
@@ -31,6 +31,21 @@ public class SecretKey {
         return Utility.mix(in, 56, mixVector);
     }
 
+    private boolean[] finalTransform(boolean[] in) {
+        assert in.length == 48 : "Длина ключа для финальной перестановки должна быть 48 бит.";
+
+        byte[] mixVector = new byte[] {
+                14, 17, 11, 24,  1,  5,  3, 28,
+                15,  6, 21, 10, 23, 19, 12,  4,
+                26,  8, 16,  7, 27, 20, 13,  2,
+                41, 52, 31, 37, 47, 55, 30, 40,
+                51, 45, 33, 48, 44, 49, 39, 56,
+                34, 53, 46, 42, 50, 36, 29, 32
+        };
+
+        return Utility.mix(in, 48, mixVector);
+
+    }
 
 
 
