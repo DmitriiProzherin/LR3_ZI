c1f4a3ea47104bd5a63432c53fd907568ee4c48b
BehemothDi <dmitriiprozherin@mail.ru>
2022-12-19 04:05:41 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         String strKey = "0001001100110100010101110111100110011011101111001101111111110001";
         String messageBlock = "0000000100100011010001010110011110001001101010111100110111101111";
+        String expectedEncryptedBlock = "1000010111101000000100110101010000001111000010101011010000000101";
 
         boolean[] b_key = new boolean[64];
         boolean[] b_message_block = new boolean[64];
@@ -20,26 +21,12 @@ public class Main {
         SecretKey key = new SecretKey(b_key);
         DES des = new DES();
 
-      //  key.print_56_bit_key();
-        //key.printKeysArr();
 
-//        boolean[] test_bool = new boolean[] {true, false, true, false, true, false, true};
-//
-//        Utilities.Utility.shiftLeft(test_bool, 0);
-//
-//        System.out.println(Arrays.toString(test_bool));
-
-//        System.out.println(Arrays.toString(b_key));
-//        System.out.println(Arrays.toString(key.initTransform(b_key)));
 
         des.encryptBlock(b_message_block, key);
-
+        System.out.println(expectedEncryptedBlock);
 
        // des.decrypt(text, key);
-
-//        boolean[] test = new boolean[] {true, false, true, false, false, true};
-//        ArrayList<boolean[]> testList = des.splitBlockIntoParts(test, 2);
-//        testList.forEach(m -> System.out.println(Arrays.toString(m)));
 
 
     }
